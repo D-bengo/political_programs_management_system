ea4dc46ea2c174769d634471cf10213dfe58df67
D-bengo <kichodenis31@gmail.com>
2026-06-04 10:03:19 +0300

    Done some few changes

--- political-frontend/src/api/axios.js
+++ political-frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:5555",
+  baseURL: "https://political-programs-management-system-1.onrender.com",
+  withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
